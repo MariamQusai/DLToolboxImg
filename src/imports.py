313301803ec4c9930edd1afcb501d2f07adfb04c
x110 <masagban@gmail.com>
2018-11-05 08:51:51 +0400

    Trained model on larger data set

---
+++ src/imports.py
@@ -0,0 +1,13 @@
+import pylidc as pl #pip install -Iv scikit-image==0.13
+from random import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import scipy
+import random
+import matplotlib.patches as patches
+import matplotlib.animation as animation
+from IPython.display import HTML
+from helpers import *
+from ctscan import *
